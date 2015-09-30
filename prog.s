1bad433396e489d5396315fbee62c2fee59b419b
drkitty <bumblingkitty@gmail.com>
2015-09-30 15:02:08 -0700

    Flip LEDs super quickly

--- prog.s
+++ prog.s
@@ -2,8 +2,13 @@
 
 .text
 
-start:      ldi r16, 0xFF
-            out 0x17, r16
-            ldi r16, 0x55
-            out 0x18, r16
-loop:       jmp loop
+start:		ldi r16, 0xFF ; all output / loop index / flip all bits
+			out 0x17, r16 ; DDRB
+			ldi r17, 0x55 ; 0n01010101
+
+flip:		eor r17, r16
+			out 0x18, r17 ; PORTB
+loop2:		dec r16
+			brne loop2
+			ldi r16, 0xFF ; loop index
+			jmp flip
